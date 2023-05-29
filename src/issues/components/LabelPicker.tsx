58d82b639bfef8bbb6f4bcabceaab672a3010adf
Daniel Mateu <danielmateu86@gmail.com>
2023-05-29 16:31:29 +0200

    instalamos axios

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,14 +1,29 @@
+import { useQuery } from '@tanstack/react-query'
+
+const getLabels = async () => {
+  const response = await fetch('https://api.github.com/repos/facebook/react/labels')
+  const data = await response.json()
+
+  return data
+}
 
 export const LabelPicker = () => {
+
+
+  const labelsQuery = useQuery(['labels'], getLabels)
+  
+
+
+
   return (
     <div>
-        <span 
-            className="badge rounded-pill m-1 label-picker"
-            style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
-        >
-            Primary
-        </span>
-        
+      <span
+        className="badge rounded-pill m-1 label-picker"
+        style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
+      >
+        Primary
+      </span>
+
     </div>
   )
 }
