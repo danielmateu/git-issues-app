0d6b6ce7c090e314e9729828b63770a166be2d3e
Daniel Mateu <danielmateu86@gmail.com>
2023-05-29 16:41:26 +0200

    creacion del tipo de dato Label

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,18 +1,21 @@
 import { useQuery } from '@tanstack/react-query'
+import { githubApi } from '../../api/githubApi'
+import { Label } from '../interfaces/label'
 
-const getLabels = async () => {
-  const response = await fetch('https://api.github.com/repos/facebook/react/labels')
-  const data = await response.json()
 
+
+const getLabels = async (): Promise<Label[]> => {
+  const { data } = await githubApi.get<Label[]>('/labels')
+  
   return data
 }
 
 export const LabelPicker = () => {
 
 
-  const labelsQuery = useQuery(['labels'], getLabels)
-  
-
+  const labelsQuery = useQuery(['labels'], getLabels, {
+    refetchOnWindowFocus: false,
+  })
 
 
   return (
