9e76f551f868d0f4cea85032c5c4d042df1f40cb
Daniel Mateu <danielmateu86@gmail.com>
2023-05-31 13:34:46 +0200

    initial data y placeholder data

--- src/hooks/useLabels.tsx
+++ src/hooks/useLabels.tsx
@@ -13,9 +13,31 @@ export const useLabels = () => {
     }
     const labelsQuery = useQuery(['labels'], getLabels,
         {
-            refetchOnWindowFocus: false,
+            // refetchOnWindowFocus: false,
             staleTime: 1000 * 60 * 60, // 1 hour
-            
+            // initialData: [],
+            // placeholderData: [],
+            initialData: [
+                {
+                    id: 725156255,
+                    node_id: "MDU6TGFiZWw3MjUxNTYyNTU=",
+                    url: "https://api.github.com/repos/facebook/react/labels/good%20first%20issue%20(taken)",
+                    name: "good first issue (taken)",
+                    color: "b60205",
+                    default: false,
+                    description: "Claimed by a contributor",
+                },
+                {
+                    id: 717031390,
+                    node_id: "MDU6TGFiZWw3MTcwMzEzOTA=",
+                    url: "https://api.github.com/repos/facebook/react/labels/good%20first%20issue",
+                    name: "good first issue",
+                    color: "6ce26a",
+                    default: true,
+                    description: null,
+                }
+            ],
+
         }
     )
 
