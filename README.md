7dd85a33859eccc27fe473e2564e028c1b97657d
Daniel Mateu <danielmateu86@gmail.com>
2023-06-08 17:03:30 +0200

    Done!

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 3. Abrir el URL del proyecto
 
 !ACTUALIZAR EL ACCES TOKEN 🌟🌟🌟 -> Settings // Developer settings // Personal access tokens
+
+Proyecto creado a partir del curso gratuito creado por Fernando Herrera de React Query, alojado en su plataforma DEVTALLES. Para más información: https://cursos.devtalles.com/
--- src/api/githubApi.ts
+++ src/api/githubApi.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 export const githubApi = axios.create({
     baseURL: 'https://api.github.com/repos/facebook/react',
     headers: {
-        Authorization: 'Bearer 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'
+        Authorization: 'Bearer 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'
     }
 })
 
